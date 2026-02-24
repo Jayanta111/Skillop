0098d67e23976bbc6086af56a6955635ce99635c
Jayanta111 <jayantachungkrang3@gmail.com>
2026-02-24 21:10:46 +0530

    upadted

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://sociallybackend12.vercel.app/";
+export const BASE_URL = "https://sociallybackend.onrender.com";
 export const clientServer=axios.create({
     baseURL:BASE_URL,
 })
